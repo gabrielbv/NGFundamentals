1c676f1d0c3cfd88d3efa40705520054711b3052
Gabriel Dianu <gabriel.dianu@gmail.com>
2019-10-07 15:39:00 +0300

    angular project commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
