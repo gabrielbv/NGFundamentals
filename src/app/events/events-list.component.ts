51d0cd54140cfc46c54fccf393079dd75229bfba
Gabriel Dianu <gabriel.dianu@gmail.com>
2019-10-07 16:09:10 +0300

    save before leaving to work

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
     <div>
       <h1>Upcoming Angular Events</h1>
       <hr>
-      <event-thumbnail [event]="event1"></event-thumbnail>
+      <div class= "well">
+        <div>Hello World</div>
+      </div>
+      <event-thumbnail  [event]="event1"></event-thumbnail>
+
   </div>
 `
 })
@@ -26,7 +30,6 @@ export class EventsListComponent {
       country: 'England'
     }
   };
-
 
 
 }
