a37951cb1e1025de09484fa6860c96da440af69b
Gabriel <gabriel.dianu@gmail.com>
2019-11-06 17:11:43 +0200

    chapter 3 routing and navigation

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -4,12 +4,23 @@ import { Component } from '@angular/core';
   // tslint:disable-next-line:component-selector
   selector: 'nav-bar',
   templateUrl: './navbar.component.html',
-  styles: [ `
-    .nav.navbar-nav {font-size: 15px;}
-    #searchForm {margin-right: 100px}
-    @media (max-width:1200px) {#searchForm {display:none}}
-  `]
+  styles: [
+    `
+      .nav.navbar-nav {
+        font-size: 15px;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none;
+        }
+      }
+      li > a.active {
+        color: #f97924;
+      }
+    `
+  ]
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}
